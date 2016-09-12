bc850548d28b0518e30ef1870f5de0e297762a12
hzyouzhihao <hzyouzhihao@corp.netease.com>
2016-09-12 14:51:20 +0800

    编写一个strClient 1.包含解码器和编码器

---
+++ src/main/java/netty/demo3/ServerHandler.java
@@ -0,0 +1,21 @@
+package netty.demo3;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by hzyouzhihao on 2016/9/12.
+ */
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        Message message =(Message)msg;
+        System.out.println(message.getContent());
+    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
